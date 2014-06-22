1077d2e38a6fd3791085a17928e05d43682028b4
Jason Flogel <jason.flogel@gmail.com>
2014-06-22 14:06:21 -0500

    created first fitnesse test

--- fitnesse/run.sh
+++ fitnesse/run.sh
@@ -1,1 +1,1 @@
-java -jar fitnesse-standalone.jar -p 9001
+java -jar fitnesse-standalone.jar -p 9001 -e 0
---
+++ src/test/groovy/address/book/MyFixture.groovy
@@ -0,0 +1,15 @@
+package address.book
+
+import fitlibrary.DoFixture
+
+
+public class MyFixture extends DoFixture {
+
+    public MyFixture() {
+
+    }
+
+    boolean navigateTo(String url) {
+        true
+    }
+}
